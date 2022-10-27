1683f01ec9c307e65ec296bcb9e5d20a4a915b95
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-10-27 18:24:50 -0300

    Desafio Studly Caps Ruby

--- lib/string_utils.rb
+++ lib/string_utils.rb
@@ -1,4 +1,12 @@
 class StringUtils
   def wavefy(string)
+    for a in 0...string.length do
+      if a%2 == 1
+        string[a] = string[a].upcase
+      else
+        string[a] = string[a].downcase
+      end
+    end
+    string
   end
 end
